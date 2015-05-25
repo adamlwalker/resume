e014f240d7790cfc55bbe43dd0604bc0f27e8d3f
Adam Walker <adamlwalker77@gmail.com>
2015-05-25 15:04:30 -0400

    Created nested forms

--- app/admin/resume.rb
+++ app/admin/resume.rb
@@ -3,15 +3,31 @@ ActiveAdmin.register Resume do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
- permit_params :name, :company, :position 
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+form do |f|
+	f.inputs 'Resume' do
+	f.input :name
+	f.input :company
+	f.input :position
+		f.inputs 'Details' do
+		f.has_many :detail, :allow_destroy => true, :new_record => true do |t|
+			t.input :summary
+			t.input :about
+			t.input :extra
+		end
+	end
+	f.inputs 'Skills' do
+		f.has_many :skills, :allow_destroy => true, :new_record => true do |t|
+			t.input :name
+			t.input :level
+		end
+	end
+	f.inputs 'Services' do
+		f.has_many :services, :allow_destroy => true, :new_record => true do |t|
+			t.input :name
+			t.input :level
+		end
+	end
 
-
+end
+end
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,8 @@
 class Resume < ActiveRecord::Base
 has_many :skills
-
+has_many :services
+has_one :detail
+accepts_nested_attributes_for :skills
+accepts_nested_attributes_for :services
+accepts_nested_attributes_for :detail
 end
