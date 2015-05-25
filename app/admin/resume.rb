4109eab104ac0f25e7607614ed50dd14bba9b4d6
Adam Walker <adamlwalker77@gmail.com>
2015-05-25 15:46:47 -0400

    Removed uneeded editor

--- app/admin/resume.rb
+++ app/admin/resume.rb
@@ -8,26 +8,42 @@ form do |f|
 	f.input :name
 	f.input :company
 	f.input :position
-		f.inputs 'Details' do
+	f.inputs 'Resume Details' do
 		f.has_many :detail, :allow_destroy => true, :new_record => true do |t|
 			t.input :summary
 			t.input :about
 			t.input :extra
 		end
-	end
-	f.inputs 'Skills' do
+
+
+		f.has_many :experiences, :allow_destroy => true, :new_record => true do |t|
+			t.input :employer
+			t.input :startend
+			t.input :jobtitle
+			t.input :jobdescription
+		end
+
 		f.has_many :skills, :allow_destroy => true, :new_record => true do |t|
 			t.input :name
 			t.input :level
 		end
-	end
-	f.inputs 'Services' do
+
+
 		f.has_many :services, :allow_destroy => true, :new_record => true do |t|
 			t.input :name
 			t.input :level
 		end
-	end
+	
+	
+		f.has_many :softwares, :allow_destroy => true, :new_record => true do |t|
+			t.input :name
+			t.input :level
+		end
 
-end
+		f.has_many :hobbies, :allow_destroy => true, :new_record => true do |t|
+			t.input :name
+		end
+	end
+end	
 end
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,8 +1,14 @@
 class Resume < ActiveRecord::Base
 has_many :skills
 has_many :services
+has_many :softwares
 has_one :detail
+has_many :experiences
+has_many :hobbies
 accepts_nested_attributes_for :skills
 accepts_nested_attributes_for :services
 accepts_nested_attributes_for :detail
+accepts_nested_attributes_for :softwares
+accepts_nested_attributes_for :experiences
+accepts_nested_attributes_for :hobbies
 end
